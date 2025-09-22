1f0c4be8e4c7fb0e80ed2cc69b8a07cf58da930c
atcupps <91998663+atcupps@users.noreply.github.com>
2025-09-22 13:44:36 -0400

    remove build artifacts from src folder

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { JupiterpClientV0 } from "./api/client";
-export { ApiResponse } from "./api/responses";
-export { SortBy } from "./api/sort-by";
-export { CreditFilter, RatingFilter } from "./api/api-filters";
-export { GenEd } from "./common/course-traits";
